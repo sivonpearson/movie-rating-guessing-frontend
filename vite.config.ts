00ae1678fff3aadee25b17a81aaee5292322b50a
sivonpearson <sivonharris@gmail.com>
2025-02-25 14:18:31 -0600

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/movie-rating-guessing-frontend",
+  base: "/movie-rating-guessing-frontend/",
 });
